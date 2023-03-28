9f7fbeda5de7214bf370fc819126eb46ccf40218
Ozsanlav-Harris <lo288@rstudio03n.isad.isadroot.ex.ac.uk>
2023-03-28 13:49:22 +0100

    Update spatial data for maps

---
+++ R code/X_Create maps for publication.R
@@ -0,0 +1,11 @@
+## Luke Ozsanlav-Harris
+
+## Created: 28/03/2023
+
+## Create various maps for publication to show Islay location, habitat and roads
+## Create Figure 1: map of Islay with RAMSAR and GBG roost sites marked and entire UK as inlay
+## Create sup figure: map of Islay with all habitat shown
+## Create sup figure: map of Islay with only agricultural land and roads shown
+
+## Load required packages
+pacman::p_load(tidyverse, data.table, sf)
